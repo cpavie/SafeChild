8607851ff9c9894cd4ea4aa9a6a7c1358b9a30ed
cpavie <pavieclaudio@gmail.com>
2021-03-16 23:09:16 -0300

    login guard mejorado

--- src/app/guards/is-logged.guard.ts
+++ src/app/guards/is-logged.guard.ts
@@ -1,34 +1,28 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { Observable } from 'rxjs';
 import { AngularFireAuth } from "@angular/fire/auth";
+import { take, switchMap } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class IsLoggedGuard implements CanActivate {
-  constructor(private AFA:AngularFireAuth,
+  constructor(private auth:AngularFireAuth,
     private router: Router){
-
   }
-  state:number
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    this.AFA.authState.subscribe(res=>{
-      if (res && res.uid) {
-        this.state = 1
-      }else{
-        this.state = 0
-      }
-    })
-
-    if (this.state == 1) {
-      return true
-    }else{
-      this.router.navigate(['/home'])
-      return false
+  canActivate() {
+        return this.auth.authState.pipe(
+            take(1),
+            switchMap(async (authState) => {
+                if (authState) { // check are user is logged in
+                    return true
+                } else {
+                    this.router.navigate(['/home'])
+                    return false
+                }
+            }),
+        )
     }
-  }
   
 }
